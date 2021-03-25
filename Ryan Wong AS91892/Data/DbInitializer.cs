a7f3cdc08aa76025f6a9a2074547f097a6bbfa9e
Ryan Wong <ac117731@avcol.school.nz>
2021-03-26 10:27:36 +1300

    Finalised the dbInitialiser and added the doccumentation

--- Ryan Wong AS91892/Data/DbInitializer.cs
+++ Ryan Wong AS91892/Data/DbInitializer.cs
@@ -22,19 +22,24 @@ namespace Ryan_Wong_AS91892.Data
 
             var Stores = new Store[]
            {
-                new Store{Location="Hell"},
-                new Store{Location="Somewhere"}
+                new Store{Location="Cloud City"},
+                new Store{Location="Hyrule"},
+                new Store{Location="Horizon Lunar Colony"},
+                new Store{Location="Snowdin"},
+                new Store{Location="New Donk City"}
            };
 
             context.Stores.AddRange(Stores);
             context.SaveChanges();
 
-
             var Items = new Item[]
             {
-                new Item {Name="Gun",Description="Bang!",Price=12,ItemType="Weapon",StoreID=2},
-                new Item {Name="Knife",Description="ow",Price=11,ItemType="Weapon",StoreID=2},
-                new Item {Name="Strong potion",Description="you cannot handle my potions traveler",Price=5,ItemType="Potion",StoreID=1}
+                new Item {Name="Buster Sword",Description="Big Sword",Price=12,ItemType="Sword",StoreID=2},
+                new Item {Name="Hawkmoon",Description="Holding Aces",Price=25,ItemType="Hand Cannon",StoreID=3},
+                new Item {Name="Chaos Shield",Description="Bulls Rush Has Extended Range",Price=25,ItemType="Shield",StoreID=3},
+                new Item {Name="Spartan Lazer",Description="Long Range Charge Rifle",Price=25,ItemType="Railgun",StoreID=3},
+                new Item {Name="Gjallarhorn",Description="Wolfpack Round Release After Detonation",Price=35,ItemType="Rocket Launcher",StoreID=5},
+                new Item {Name="Max Revive",Description="Revives A Fallen Ally To Full Health",Price=50,ItemType="Potion",StoreID=1}
 
             };
 
@@ -43,9 +48,10 @@ namespace Ryan_Wong_AS91892.Data
 
             var Staffs = new Staff[]
             {
-                new Staff{FirstName="Gus", LastName="Carsons",HireDate=DateTime.Parse("2019-09-01"),Wages=5, StoreID=1 },
-                new Staff{FirstName="Steve", LastName="Creggory",HireDate=DateTime.Parse("2018-02-14"),Wages=10,StoreID=2},
-                new Staff{FirstName="Guy", LastName="Jones",HireDate=DateTime.Parse("2015-10-24"),Wages=2,StoreID=1}
+                new Staff{FirstName="Gus", LastName="Carsons",HireDate=DateTime.Parse("2019-09-01"),Wages=8, StoreID=1 },
+                new Staff{FirstName="Winston", LastName=" ",HireDate=DateTime.Parse("2016-05-24"),Wages=5,StoreID=3},
+                new Staff{FirstName="Banshee", LastName="44",HireDate=DateTime.Parse("2010-02-12"),Wages=25,StoreID=5},
+                new Staff{FirstName="Temmi", LastName="e",HireDate=DateTime.Parse("2015-10-24"),Wages=15,StoreID=4}
             };
 
             context.Staffs.AddRange(Staffs);
